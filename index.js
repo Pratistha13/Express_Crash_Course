f49b191fce95c8b983916325c0172eb5ca6d6bf3
Pratistha Mishra <56651666+Pratistha13@users.noreply.github.com>
2023-06-27 11:37:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+//const logger = require("./middleware/logger");
+
+const app = express();
+
+//Init middleware
+//app.use(logger);
+
+//Body Parser Middlware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public"))); //We use app.use when we want to use a middleware
+
+// app.get('/', (req,res)=> {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
---
+++ routes/api/members.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+
+const members = require('../../Members');
+
+//Get All members
+router.get("/", (req, res) => res.json(members));
+
+//Get single member
+router.get("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+//Create a Member
+// Create Member
+router.post('/', (req, res) => {
+    const newMember = {
+      ...req.body,
+      id: uuid.v4(),
+      status: 'active'
+    };
+  
+    if (!newMember.name || !newMember.email) {
+      return res.status(400).json({ msg: 'Please include a name and email' });
+    }
+  
+    members.push(newMember);
+    res.json(members);
+    // res.redirect('/');
+  });
+
+module.exports= router;
